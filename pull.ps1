fa20254d08347ab24fb657beb4dee1d459b66e9d
Hunter Kraft <hek@braemail.us>
2026-04-17 10:53:25 -0400

    Use git -C flag to set working directory explicitly

--- pull.ps1
+++ pull.ps1
@@ -1,3 +1,2 @@
-cd C:\MPP
-& "C:\Program Files\Git\cmd\git.exe" fetch origin hunter/explore
-& "C:\Program Files\Git\cmd\git.exe" reset --hard origin/hunter/explore
+& "C:\Program Files\Git\cmd\git.exe" -C C:\MPP fetch origin hunter/explore
+& "C:\Program Files\Git\cmd\git.exe" -C C:\MPP reset --hard origin/hunter/explore
